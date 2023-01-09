e384c9e1c3d1e6ae1774066bafea493becb53948
JanKolo04 <jankolodziej04@gmail.com>
2023-01-09 20:28:07 +0100

    create mute function and unmute

--- command-handling.js
+++ command-handling.js
@@ -1,5 +1,8 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('fs');
+const { toASCII } = require('punycode');
+const ascii = require('ascii-table');
+const table = new ascii().setHeading('Commands', 'Status');
 
 const commands = [];
 
@@ -12,14 +15,19 @@ for(const folder of fs.readdirSync('./commands')) {
     }
 }
 
-const rest = new REST({ version: '10' }).setToken(process.env.CLIENT_TOKEN);
+const rest = new REST({ version: '10' }).setToken('client token');
 
 (async() => {
     try {
-        console.log('Start downloading commands');
-        const data = await rest.put(Routes.applicationCommands(process.env.APP_TOKEN), { body: commands });
+        console.log(' Start downloading commands');
+        const data = await rest.put(Routes.applicationCommands('app token'), { body: commands });
 
-        console.log('Downloading has been finished');
+        //print loaded commands
+        for(let i=0; i<commands.length; i++) {
+            table.addRow(commands[i].name, "loaded");
+        }
+
+        console.log(table.toString(), '\n Downloading has been finished')
     }
     catch(error) {
         console.error(error);
